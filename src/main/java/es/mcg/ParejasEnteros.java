ca958e88c12269de0a5c5cad9ac8c8d02928d85d
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-10-21 21:08:56 +0200

    Ejercicio de parejas de numeros

--- src/main/java/es/mcg/ParejasEnteros.java
+++ src/main/java/es/mcg/ParejasEnteros.java
@@ -22,23 +22,72 @@ public class ParejasEnteros {
             }
             fileOutputStream = new FileOutputStream(file);
             dataOutputStream = new DataOutputStream(fileOutputStream);
-            String entrada;
-            int salida;
-            System.out.println("Introduzca un entero. INTRO para cerrar el programa");
-            entrada = sc.nextLine();
-            while(!entrada.equals("INTRO"))
-            {
-                salida = Integer.parseInt(entrada);
-                if(Integer.MAX_VALUE < salida)
+            String entrada1, entrada2;
+            int salida1, salida2;
+            System.out.println("Para cerrar el programa, escriba INTRO");
+            do{
+                System.out.println("Introduce un numero entero");
+                entrada1 = sc.nextLine();
+                if(entrada1.equals("INTRO"))
                 {
-                    dataOutputStream.writeInt(salida);
+                    System.out.println("Ha finalizado el programa");
+                    cerrarFlujos(fileOutputStream, dataOutputStream);
+                    System.exit(0);
                 }
-                entrada = sc.nextLine();
-            }
+                System.out.println("Introduce otro numero entero");
+                entrada2 = sc.nextLine();
+                if(entrada2.equals("INTRO"))
+                {
+                    System.out.println("Ha finalizado el programa");
+                    cerrarFlujos(fileOutputStream, dataOutputStream);
+                    System.exit(0);
+                }
+                if(entrada1.equals(entrada2))
+                {
+                    salida1 = Integer.parseInt(entrada1);
+                    salida2 = Integer.parseInt(entrada2);
+                    if(Integer.MAX_VALUE > salida1 || Integer.MAX_VALUE > salida2)
+                    {
+                        dataOutputStream.writeInt(salida1);
+                        dataOutputStream.writeInt(salida2);
+                    }
+                    else
+                    {
+                        System.err.println("ERROR. Valor del numero excedido");
+                    }
+                }
+                else
+                {
+                    System.err.println("ERROR. No son parejas");
+                }
+            }while(!entrada1.equals("INTRO") || !entrada2.equals("INTRO"));
+            //String entrada;
+            //int salida;
+            //System.out.println("Introduzca un entero. INTRO para cerrar el programa");
+            //entrada = sc.nextLine();
+            //while(!entrada.equals("INTRO"))
+            //{
+            //    salida = Integer.parseInt(entrada);
+            //    if(Integer.MAX_VALUE < salida)
+            //    {
+            //        dataOutputStream.writeInt(salida);
+            //    }
+            //    entrada = sc.nextLine();
+            //}
         } 
         catch (IOException ioException) 
         {
             ioException.printStackTrace();
         }
+    }
+
+    private static void cerrarFlujos(FileOutputStream fileOutputStream, DataOutputStream dataOutputStream){
+                try{
+                    dataOutputStream.close();
+                    fileOutputStream.close();
+                }catch(IOException ioException){
+                    ioException.printStackTrace();
+                }
+        
     }
 }
